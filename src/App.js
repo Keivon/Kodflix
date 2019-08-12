cb0cc9588c65e14ccf3dd279d7b371bd54c92d69
Keivon <keivonproctor@hotmail.com>
2019-08-12 10:08:03 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
+import friends from '../src/images/Friends from College.jpg';
+import smallville from '../src/images/Smallville.jpg';
+import sherlock from '../src/images/sherlock.jpg';
+import southPart from '../src/images/South Park.jpg';
+import stranger_things from '../src/images/stranger things.jpg';
+import Luke_Cage from '../src/images/Luke Cage.jpg'; 
 import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+    <div class="flex-container">
+
+            <div class="flex-item">
+            
+            <img src={friends}  alt='Friends from College' />
+             </div>
+             
+            <div class="flex-item">
+            
+            <img src={smallville}  alt='smallville' />
+            </div>
+
+            <div class="flex-item">
+           
+            <img src={sherlock}  alt='' />
+            </div>
+          </div>
+          <div class="flex-container">
+
+                <div class="flex-item">
+                
+                <img src={southPart}  alt='' />
+                </div>
+
+                <div class="flex-item">
+                
+                <img src={stranger_things}  alt='stranger_things' />
+                </div>
+
+                <div class="flex-item">
+                
+                <img src={Luke_Cage} resizeMode="stretch" alt='Luke Cage' />
+                </div>
+
+        </div>
+     </div>
+   
   );
 }
 
