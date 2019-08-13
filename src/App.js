c15cf1bb0ae7051aa97fca4fbe8a0a801849be3c
Keivon <keivonproctor@hotmail.com>
2019-08-13 12:23:18 +0100

    At task #7

--- src/App.js
+++ src/App.js
@@ -15,35 +15,36 @@ function App() {
     <div class="flex-container">
 
             <div class="flex-item">
-            
+            <div class="overlay"><h1>Friends from College</h1></div>
             <img src={friends}  alt='Friends from College' />
-             </div>
+            </div>
              
             <div class="flex-item">
-            
+            <div class="overlay"><h1>Smallville</h1></div>
             <img src={smallville}  alt='smallville' />
             </div>
 
             <div class="flex-item">
-           
-            <img src={sherlock}  alt='' />
+            <div class="overlay"><h1>Sherlock</h1></div>
+            <img src={sherlock}  alt='Sherlock' />
             </div>
+
           </div>
           <div class="flex-container">
 
                 <div class="flex-item">
-                
-                <img src={southPart}  alt='' />
+                 <div class="overlay"><h1>South Part</h1></div>
+                <img src={southPart}  alt='South Part' />
                 </div>
 
                 <div class="flex-item">
-                
+                <div class="overlay"><h1>Stranger Things</h1></div>
                 <img src={stranger_things}  alt='stranger_things' />
                 </div>
 
                 <div class="flex-item">
-                
-                <img src={Luke_Cage} resizeMode="stretch" alt='Luke Cage' />
+                <div class="overlay"><h1>Luke Cage</h1></div>
+                <img src={Luke_Cage}  alt='Luke Cage' />
                 </div>
 
         </div>
