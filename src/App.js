7951deb8548ea52490e355a0f05ea45c285970a2
Keivon <keivonproctor@hotmail.com>
2019-08-20 16:06:54 +0100

    first real changes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,13 @@
 import React from 'react';
-import Tvcover from './Tvcover';
-import friends from '../src/images/Friends from College.jpg';
-import smallville from '../src/images/Smallville.jpg';
-import sherlock from '../src/images/sherlock.jpg';
-import southPart from '../src/images/South Park.jpg';
-import stranger_things from '../src/images/stranger things.jpg';
-import Luke_Cage from '../src/images/Luke Cage.jpg'; 
+import Gallery from './Gallery.js';
 import './App.css';
 
 function App() {
   return (
     
     <div className="App">
-   
-          <div class="flex-container">
-
-            <Tvcover title="Friends from College" img={friends}/>
-            <Tvcover title="Smallville" img={smallville}/>
-            <Tvcover title="Sherlock" img={sherlock}/>
-          
-          </div>
-         <div class="flex-container">
-
-          <Tvcover title="South Part" img={southPart}/>
-          <Tvcover title="Stranger Things" img={stranger_things}/>
-          <Tvcover title="Luke Cage" img={Luke_Cage}/>
-
-         </div>
-     </div>
-   
+       <Gallery/>
+    </div>
   );
 }
 
--- src/Tvcover.js
+++ src/Tvcover.js
@@ -5,8 +5,8 @@ export default function Tvcover(props) {
 
     return (
             <div class="flex-item">
-            <div class="overlay"><h1>{props.title}</h1></div>
-            <img src={props.img}  alt={props.title}/>
+                <div class="overlay"><h1>{props.title}</h1></div>
+                <img src={props.img}  alt={props.title}/>
             </div>
     );
 }
