9fdd7a98178c6b970fe1b96ef78b7247fd75bc94
Keivon <keivonproctor@hotmail.com>
2019-09-10 11:04:59 +0100

    Task #23

--- src/backend/App.js
+++ src/backend/App.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 const getShows = require('./rest/shows');
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '../../build')));
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+});
 
 app.get('/rest/shows', (req, res) => res.send(getShows()));
 
